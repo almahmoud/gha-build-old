fd6f7e166998dac52a6331a1aea9a31f252c99c6
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2022-09-10 00:45:08 -0400

    From source

--- .github/scripts/build_package.sh
+++ .github/scripts/build_package.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-set -x
+set -xe
 git pull origin main || git reset --hard origin/main
 LIBRARY=$1
 PKG=$2
-mkdir -p built/
-mkdir -p tars/
-mkdir -p logs/
-mkdir -p /tmp/built/
+mkdir -p $LIBRARY
 mkdir -p /tmp/tars/
 mkdir -p /tmp/logs/
-Rscript -e "p <- .libPaths(); p <- c('$LIBRARY', p); .libPaths(p); if(BiocManager::install('$PKG', INSTALL_opts = '--build', update = TRUE, quiet = TRUE, force = TRUE, keep_outputs = TRUE) %in% rownames(installed.packages())) q(status = 0) else q(status = 1)" && cp *.tar.gz /tmp/tars/ && cp *.out /tmp/logs/ && cp -r "built/$PKG" /tmp/built/
+sed -n "/^    \"$PKG\"/,/^    \"/p" packages.json | grep '^        "' | awk -F'"' '{print $2}' | xargs -i cat lists/{} | xargs -i echo "curl -o $LIBRARY/{} https://js2.jetstream-cloud.org:8001/swift/v1/gha-build/{}"
+( cd $LIBRARY && ls | grep ".tar.gz" | xargs -i tar -xvf {} )
+if git clone git@git.bioconductor.org:packages/$PKG
+then
+	Rscript -e "p <- .libPaths(); p <- c('$LIBRARY', p); .libPaths(p); if(BiocManager::install('./$PKG', INSTALL_opts = '--build', update = TRUE, quiet = TRUE, force = TRUE, keep_outputs = TRUE) %in% rownames(installed.packages())) q(status = 0) else q(status = 1)"
+else
+	Rscript -e "Sys.setenv(BIOCONDUCTOR_USE_CONTAINER_REPOSITORY=FALSE); p <- .libPaths(); p <- c('$LIBRARY', p); .libPaths(p); if(BiocManager::install('$PKG', INSTALL_opts = '--build', update = TRUE, quiet = TRUE, force = TRUE, keep_outputs = TRUE) %in% rownames(installed.packages())) q(status = 0) else q(status = 1)"
+fi
+cp *.tar.gz /tmp/tars/
+cp *.out /tmp/logs/
--- .github/scripts/rclone_push.sh
+++ .github/scripts/rclone_push.sh
@@ -3,6 +3,12 @@ set -x
 git pull origin main || git reset --hard origin/main
 
 PKGTOMARK=$1
-sed -i 's/built/pushed/g' "lists/$PKGTOMARK"
+TAR=$2
+sed -i "s#built#$TAR#g" "lists/$PKGTOMARK"
+sed -i "s/    \"$PKGTOMARK\": \[\],\?//g" packages.json
+sed -i "s/        \"$PKGTOMARK\",\?//g" packages.json
+sed -i -z 's/,\n\n\+}/}/g' packages.json
+python -c 'import json; f = open("packages.json", "r"); pkgs = json.load(f); f.close(); f = open("packages.json", "w"); f.write(json.dumps(pkgs, indent=4)); f.close()'
 git add lists
+git add packages.json
 git commit -m "Mark pushed $PKGTOMARK"
