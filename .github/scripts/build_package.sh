9259079da9721cfcf0d0b6ced116aa5b6aed50dd
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2022-09-10 01:22:31 -0400

    Start automatic

--- .github/scripts/build_package.sh
+++ .github/scripts/build_package.sh
@@ -6,13 +6,13 @@ PKG=$2
 mkdir -p $LIBRARY
 mkdir -p /tmp/tars/
 mkdir -p /tmp/logs/
-sed -n "/^    \"$PKG\"/,/^    \"/p" packages.json | grep '^        "' | awk -F'"' '{print $2}' | xargs -i cat lists/{} | xargs -i echo "curl -o $LIBRARY/{} https://js2.jetstream-cloud.org:8001/swift/v1/gha-build/{}"
+sed -n "/^    \"$PKG\"/,/^    \"/p" original.json | grep '^        "' | awk -F'"' '{print $2}' | xargs -i cat lists/{} | xargs -i echo "curl -o $LIBRARY/{} https://js2.jetstream-cloud.org:8001/swift/v1/gha-build/{}"
 ( cd $LIBRARY && ls | grep ".tar.gz" | xargs -i tar -xvf {} )
 if git clone git@git.bioconductor.org:packages/$PKG
 then
-	Rscript -e "p <- .libPaths(); p <- c('$LIBRARY', p); .libPaths(p); if(BiocManager::install('./$PKG', INSTALL_opts = '--build', update = TRUE, quiet = TRUE, force = TRUE, keep_outputs = TRUE) %in% rownames(installed.packages())) q(status = 0) else q(status = 1)"
+	Rscript -e "p <- .libPaths(); p <- c('$LIBRARY', p); .libPaths(p); if(BiocManager::install('./$PKG', INSTALL_opts = '--build', update = TRUE, quiet = FALSE, force = TRUE, keep_outputs = TRUE) %in% rownames(installed.packages())) q(status = 0) else q(status = 1)"
 else
-	Rscript -e "Sys.setenv(BIOCONDUCTOR_USE_CONTAINER_REPOSITORY=FALSE); p <- .libPaths(); p <- c('$LIBRARY', p); .libPaths(p); if(BiocManager::install('$PKG', INSTALL_opts = '--build', update = TRUE, quiet = TRUE, force = TRUE, keep_outputs = TRUE) %in% rownames(installed.packages())) q(status = 0) else q(status = 1)"
+	Rscript -e "Sys.setenv(BIOCONDUCTOR_USE_CONTAINER_REPOSITORY=FALSE); p <- .libPaths(); p <- c('$LIBRARY', p); .libPaths(p); if(BiocManager::install('$PKG', INSTALL_opts = '--build', update = TRUE, quiet = FALSE, force = TRUE, keep_outputs = TRUE) %in% rownames(installed.packages())) q(status = 0) else q(status = 1)"
 fi
 cp *.tar.gz /tmp/tars/
 cp *.out /tmp/logs/
