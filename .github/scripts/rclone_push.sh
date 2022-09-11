b72890005ecb5bc8b50abddfcce7e01c33a8be22
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2022-09-11 01:24:07 -0700

    Update rclone_push.sh

--- .github/scripts/rclone_push.sh
+++ .github/scripts/rclone_push.sh
@@ -5,9 +5,9 @@ git pull origin main || git reset --hard origin/main
 PKGTOMARK=$1
 TAR=$2
 
-#rclone ls js2:/gha-build | grep "tar" | awk '{print $2}' > /tmp/tars
-#cat /tmp/tars | awk -F'_' '{print $1}' | sed 's#lists/##g' | xargs -i bash -c 'grep "{}" /tmp/tars | head -n1 > lists/{}'
-echo "$TAR" > "lists/$PKGTOMARK"
+rclone ls js2:/gha-build | grep "tar" | awk '{print $2}' > /tmp/tars
+cat /tmp/tars | awk -F'_' '{print $1}' | sed 's#lists/##g' | xargs -i bash -c 'grep "{}" /tmp/tars | head -n1 > lists/{}'
+#echo "$TAR" > "lists/$PKGTOMARK"
 sed -i "s/    \"$PKGTOMARK\": \[\],\?//g" packages.json
 sed -i "s/        \"$PKGTOMARK\",\?//g" packages.json
 sed -i -z 's/,\n\n\+}/}/g' packages.json
